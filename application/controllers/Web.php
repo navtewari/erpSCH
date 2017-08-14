b33e75a492be74c2065aa228396dcabac81b0f45
Dr. Nitin Deepak <nitin.d12@gmail.com>
2017-08-14 18:30:21 +0530

    menu updates
    menu updates

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -5,7 +5,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Web extends CI_Controller {
 
     function __construct() {
-        parent::__construct();        
+        parent::__construct();    
+        $this->load->model('my_model', 'mm');    
     }
 
     public function index() {       
@@ -14,11 +15,47 @@ class Web extends CI_Controller {
         //$this->load->view('templates/footer');
     }  
     
-    public function dashboard() {       
+    public function dashboard($active = 1, $submenu = 'index') {       
+        //$data['menu'] = $this->mm->getmenu($this->session->userdata('_status_'));
+        $data['active'] = $active;
+        
+        // fetching page according to active status
+        $data['page_'] = $this->get_page($active);
+        $data['inner_page'] = $this->get_submenu($submenu);
+        // ----------------------------------------s
+
+        $data['menu'] = $this->mm->getmenu('adm', 1);
+        $data['sub_menu'] = $this->mm->getsubmenu();
+        
         $this->load->view('templates/header');
-         $this->load->view('templates/menu');
-        $this->load->view('dashboard');
+        $this->load->view('templates/menu', $data);
+        $this->load->view('dashboard', $data);
         $this->load->view('templates/footer');
     }  
-
+    function get_page($active){
+        switch($active){
+            case 1:
+                $page_ = 'dashboard';
+                break;
+            case 2:
+                $page_ = 'master';
+                break;
+            default: 
+                $page_ = 'eror';
+        }
+        return $page_;
+    }
+    function get_submenu($submenu){
+        switch($submenu){
+            case 1:
+                $inner_page = 'dashboard';
+                break;
+            case 2:
+                $inner_page = 'master';
+                break;
+            default: 
+                $inner_page = 'index';
+        }
+        return $inner_page;
+    }
 }
--- application/models/My_model.php
+++ application/models/My_model.php
@@ -8,39 +8,17 @@ class My_model extends CI_Model {
         parent::__construct();
     }    
 
-    function get_files(){
-        $this->db->order_by('PDFID', 'desc');
-        $this->db->where('STATUS_',1);
-        $query = $this->db->get('b2_pdf');
+    function getmenu($status=''){
+        $this->db->select('a.*, b.USER_');
+        $this->db->from('menu_1 a');
+        $this->db->join('user_menu b', 'a.ID_= b.MENU');
+        $this->db->where('b.USER_', $status);
+        $query = $this->db->get();
+        //echo $this->db->last_query();
         return $query->result();
     }
-    
-    function get_recent_file(){
-        $this->db->order_by('DATE_', 'desc');
-        $this->db->limit(1, 0);
-        $this->db->where('STATUS_',1);
-        $this->db->where('PATH_<>','no-image.png');
-        $query = $this->db->get('b2_pdf');
-        return $query->result();
-    }
-    
-    function get_team(){
-        $this->db->order_by('MID', 'asc');        
-        $this->db->where('STATUS_',1);
-        $query = $this->db->get('members');
-        return $query->result();
-    }
-    
-    function get_newsdetail() {
-        $this->db->where('STATUS', 1);
-        $this->db->order_by('DATE_', 'DESC');
-        $query = $this->db->get('newsevents');
-        return $query->result();
-    }
-    
-    function get_team_byid($id_){
-        $this->db->where('MID', $id_);        
-        $query = $this->db->get('members');
+    function getsubmenu(){
+        $query = $this->db->get('menu_2_submenu');
         return $query->result();
     }
 }
