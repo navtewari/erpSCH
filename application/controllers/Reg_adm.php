8a05de8bd36e48122e7bf02ac88098cbe8aa40d6
Dr. Nitin Deepak <nitin.d12@gmail.com>
2017-08-19 11:56:18 +0530

    updates
    updates

--- application/controllers/Reg_adm.php
+++ application/controllers/Reg_adm.php
@@ -3,7 +3,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Reg_adm extends CI_Controller {
-
     function __construct() {
         parent::__construct();    
         $this->load->model('my_model', 'mm');    
