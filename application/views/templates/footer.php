6c20c302551bd2939a8bc810016b4ab0071aef44
Dr. Naveen Tewari <navtewari@gmail.com>
2017-08-20 08:50:29 +0530

    update
    update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -5,7 +5,7 @@
 </div>
 
 <!--end-Footer-part-->
-
+<!--
 <script src="<?php echo base_url('assets_/js/excanvas.min.js');?>"></script> 
 <script src="<?php echo base_url('assets_/js/jquery.min.js');?>"></script> 
 <script src="<?php echo base_url('assets_/js/jquery.ui.custom.js');?>"></script> 
@@ -29,6 +29,21 @@
 <script src="<?php echo base_url('assets_/js/jquery.dataTables.min.js');?>"></script> 
 <script src="<?php echo base_url('assets_/js/matrix.tables.js');?>"></script> 
 <script src="<?php echo base_url('assets_/js/wysihtml5-0.3.0.js');?>"></script>
+-->
+<script src="<?php echo base_url('assets_/js/jquery.min.js');?>"></script> 
+<script src="<?php echo base_url('assets_/js/jquery.ui.custom.js');?>"></script> 
+<script src="<?php echo base_url('assets_/js/bootstrap.min.js');?>"></script> 
+<script src="<?php echo base_url('assets_/js/bootstrap-colorpicker.js');?>"></script> 
+<script src="<?php echo base_url('assets_/js/bootstrap-datepicker.js');?>"></script> 
+<script src="<?php echo base_url('assets_/js/jquery.toggle.buttons.js');?>"></script> 
+<script src="<?php echo base_url('assets_/js/masked.js');?>"></script> 
+<script src="<?php echo base_url('assets_/js/jquery.uniform.js');?>"></script> 
+<script src="<?php echo base_url('assets_/js/select2.min.js');?>"></script> 
+<script src="<?php echo base_url('assets_/js/matrix.js');?>"></script> 
+<script src="<?php echo base_url('assets_/js/matrix.form_common.js');?>"></script> 
+<script src="<?php echo base_url('assets_/js/wysihtml5-0.3.0.js');?>"></script> 
+<script src="<?php echo base_url('assets_/js/jquery.peity.min.js');?>"></script> 
+<script src="<?php echo base_url('assets_/js/bootstrap-wysihtml5.js');?>"></script>
 
 <script type="text/javascript">
   // This function is called from the pop-up menus to transfer to
