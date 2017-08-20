1349fb1a9f74e31aced77cd23b27fd43359622b3
Dr. Naveen Tewari <navtewari@gmail.com>
2017-08-20 13:05:48 +0530

    up
    up

--- assets_/js/myjs.js
+++ assets_/js/myjs.js
@@ -1,25 +1,25 @@
-$(function(){
-	$( window ).on( "load", function(){
-		if($("#frmFindStudent").length != 0) {
-			$('#frmFindStudent').change();
-		}
-	});
+$(function () {
+    $(window).on("load", function () {
+        if ($("#frmFindStudent").length != 0) {
+            $('#frmFindStudent').change();
+        }
+    });
 
-	// Registration and Admission Forms scripts
-		$('#frmFindStudent').change(function(){
-			url_ = site_url_ + "/reg_adm/getstudents_for_dropdown";
-			$.ajax({
-				type: "POST",
-				url: url_,
-				success:  function(data){
-					var obj = JSON.parse(data);
-					var str_html = '';
-					for(i=0;i<obj.students_.length; i++){
-						str_html = str_html + "<option value='"+obj.students_[i].regid+"'>"+obj.students_[i].regid+" | "+obj.students_[i].FNAME+"</option>";
-					}
-					$('#cmbRegistrationID').html(str_html);
-				}
-			});
-		})
-	// ----------------------------------------
+    // Registration and Admission Forms scripts
+    $('#frmFindStudent').change(function () {
+        url_ = site_url_ + "/reg_adm/getstudents_for_dropdown";
+        $.ajax({
+            type: "POST",
+            url: url_,
+            success: function (data) {
+                var obj = JSON.parse(data);
+                var str_html = '';
+                for (i = 0; i < obj.students_.length; i++) {
+                    str_html = str_html + "<option value='" + obj.students_[i].regid + "'>" + obj.students_[i].regid + " | " + obj.students_[i].FNAME + "</option>";
+                }
+                $('#cmbRegistrationID').html(str_html);
+            }
+        });
+    })
+    // ----------------------------------------
 });
