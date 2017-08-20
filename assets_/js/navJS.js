b4cd135c5d6bdb5991cf7bb1c964dd9a836c26cb
Dr. Nitin Deepak <nitin.d12@gmail.com>
2017-08-20 13:01:07 +0530

    updates
    updates

--- assets_/js/navJS.js
+++ assets_/js/navJS.js
@@ -1,3 +1,8 @@
-$('#frmSession').load(function(){
+$(function(){
+	$( window ).on( "load", function(){
+		alert('hello');
+	});
+	$('#frmSession').load(function(){
     alert("Image loaded.");
+});
 });
