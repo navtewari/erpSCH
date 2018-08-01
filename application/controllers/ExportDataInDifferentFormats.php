51b2cf55fcb44c369f7c08edca217f4f5eff6400
Dr. Nitin Deepak <nitin.d12@gmail.com>
2018-08-01 23:45:42 +0530

    updates
    updates

--- application/controllers/ExportDataInDifferentFormats.php
+++ application/controllers/ExportDataInDifferentFormats.php
@@ -5,11 +5,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ExportDataInDifferentFormats extends CI_Controller
 {
-	
-	function __construct(argument)
-	{
-		# code...
-	}
 
 	function index(){
 
