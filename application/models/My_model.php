91133bb1e14f7007baa23011431778d37b70b829
Dr. Nitin Deepak <nitin.d12@gmail.com>
2018-08-01 23:31:06 +0530

    updates
    updates

--- application/models/My_model.php
+++ application/models/My_model.php
@@ -11,7 +11,9 @@ class My_model extends CI_Model {
         $this->load->model('My_error_model', 'error');
         // --------------------
     }
-
+    function getlogin(){
+        return $this->db->get('login')->result();
+    }
     function authenticate() {
         $this->db->select('a.*, b.name, b.CATEGORY_ID');
         $this->db->where('a.USERNAME_', $this->input->post('txtUser__'));
@@ -35,7 +37,6 @@ class My_model extends CI_Model {
             $flag_ = false;
             $this->session->set_flashdata('msg_', 'False Credentials !!');
         }
-
         return $flag_;
     }
 
--- assets_/js/myjs.js
+++ assets_/js/myjs.js
@@ -1,4 +1,3 @@
-var objreceipt;
 $(function(){
 	$(document).ajaxStart(function(){
 	    $('#loading_process').css('opacity', '1');
@@ -301,7 +300,10 @@ $(function(){
 			$("#s2id_cmbStaff span").text('Select Member');
 		});
 		$('#reload_me').click(function(){
-			reloadme();
+			$('#reload_or_not').val('yes');
+			if($('#reload_or_not').val() == 'yes'){
+				reloadme();
+			}
 		});
 		$('#cmbRegistrationID').change(function(){
 			if($('#cmbRegistrationID').val() == 'new'){
@@ -2017,7 +2019,7 @@ $(function(){
 		                	var discount_category = 'x';
 		                	var total_sibling = 0;
 		                	var total_sibling_discount_amount = 0;
-		                	/*
+		                	/* Here no need to calculate Sibling discount as this dicount is also calculated in other_discounts
 		                	if(obj.sibling_discount != null){
 		                		var sibling_arr = (obj.sibling_discount.SIBLINGS).split(',');
 		                		var sibling_length = parseInt(sibling_arr.length);
