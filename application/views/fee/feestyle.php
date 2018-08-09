b3e7c81c02f10e78800f065c53390450effe8669
Dr. Nitin Deepak <nitin.d12@gmail.com>
2018-08-09 21:53:33 +0530

    updates
    updates

--- application/views/fee/feestyle.php
+++ application/views/fee/feestyle.php
@@ -1,14 +1,22 @@
 <style>
     th{ text-align: left !important }
-    .payFee{ background: #f0f0f0; color: #009000; padding: 2px 5px; border-radius: 5px; font-size: 15px; text-align: center !important; }
-    .fee_heads_static{float: left; padding: 0px 3px; background: #ffff00; color: #000000; border:#808080 solid 1px; border-radius: 5px; font-size: 9px}
-    .fee_heads_flexi{float: left; padding: 0px 3px; background: #ffEE00; color: #000000; border:#808080 solid 1px; border-radius: 5px; font-size: 9px}
+    .icon-size{font-size: 16px; cursor: pointer; }
+    .icon-play{ cursor: pointer; font-size: 18px;}
+    .icon-lock{ cursor: pointer; font-size: 18px;}
+    .undoinvoice{background: #f0f0f0; color: #A0A0A0; padding: 2px 5px; border-radius: 5px; font-size: 16px; text-align: center !important; cursor: pointer; }
+    .payFee{ background: #f0f0f0; color: #009000; padding: 2px 5px; border-radius: 5px; font-size: 16px; text-align: center !important; cursor: pointer; }
+
+    .fee_heads_static{float: left; padding: 0px 3px; background: #ffff00; color: #000000; border:#808080 solid 1px; border-radius: 5px; font-size: 9px;}
+    .fee_heads_flexi{float: left; padding: 0px 3px; background: #ffEE00; color: #000000; border:#808080 solid 1px; border-radius: 5px; font-size: 9px;}
     .cover_heads{float: left; margin-left: 1px; margin-right: 1px; margin-bottom: 1px;}
     .highlightText{ color: #555555 !important; font-weight: bold }
-    .red_, .highlightText{ color: #ff0000 !important }
+    .red_, .highlightText{ font-size: 14px; color: #ff0000 !important }
     .dimtext, .highlightText{ font-weight: none !important }
-    .generate_invoice{ background: #f0f0f0; color: #ff0000; padding: 2px 5px; border-radius: 5px; font-size: 15px; }
-    .print_invoice {background: #f0f0f0; color: #009000; padding: 2px 5px; border-radius: 5px; font-size: 15px;}
+
+    .generate_invoice{ background: #f0f0f0; color: #ff0000; padding: 2px 5px; border-radius: 5px; font-size: 15px; cursor: pointer; }
+    .print_invoice {background: #f0f0f0; color: #009000; padding: 2px 5px; border-radius: 5px; font-size: 18px; cursor: pointer;}
+    .print_latest_receipt{ background: #f0f0f0; color: #009000; padding: 2px 5px; border-radius: 5px; font-size: 18px; cursor: pointer;}
+    
     .rear_message{color:#ff0000; text-align: center; padding: 3px;}
     .receipt_label{ float: left; font-size: 12px; padding: 3px 1px 0px 0px; }
     .receipt_content{ float: left; padding: 2px 0px; font-size: 11px; }
