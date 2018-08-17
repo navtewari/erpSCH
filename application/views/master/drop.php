cd1f4cb68cb9e775e3ab4907240eaa0a85011e4e
Dr. Nitin Deepak <nitin.d12@gmail.com>
2018-08-17 18:57:53 +0530

    updates
    updates

--- application/views/master/drop.php
+++ application/views/master/drop.php
@@ -2,8 +2,8 @@
     <?php
         $attrib_ = array(
             'class' => 'form-vertical',
-            'name' => 'frmAdmission',
-            'id' => 'frmAdmission',
+            'name' => 'frmStudentToDrop',
+            'id' => 'frmStudentToDrop',
         );
         echo form_open('#', $attrib_); 
     ?>
@@ -24,36 +24,65 @@
                                 'required' => 'required'
                             );
                             $options = array();
-                            $options['new'] = 'Select Student';
+                            $options['select'] = 'Select Student';
                         ?>
                         <?php echo form_dropdown($data, $options, ''); ?>
                     </div>
                 </div>
                 <div class="control-group">
-                	<label class="control-label">Class</span></label>
-                    <div class="controls" id="show_class_for_dop">
-                        Class here...
+                	<label class="control-label">Admitted Class</span></label>
+                    <div class="controls" id="show_class_for_drop" style="color: #ff0000; font-weight: bold">
+                        
                     </div>
                 </div>
                 <div class="control-group">
-                    <label class="control-label">Date of Admission <span style="font-size: 9px">(dd-mm-yyyy)</span></label>
-                    <div class="controls" id="show_class_for_dop">
-                        Class here...
+                    <label class="control-label">Date of Admission <span style="font-size: 9px">(dd/mm/yyyy)</span></label>
+                    <div class="controls" id="show_DOA" style="color: #0000ff; font-weight: bold">
+                        
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label">Reason to drop this student <sup style="color: #ff0000">*</sup></label>
+                    <div class="controls" id="show_DOA" style="color: #0000ff; font-weight: bold">
+                        <?php 
+                        $data = array(
+                            'name' => 'txtReasonToDrop',
+                            'id' => 'txtReasonToDrop',
+                            'required' => 'required',
+                            'class' => 'span11',
+                            'style' => 'height: 100px'
+                        );
+                        echo form_textarea($data);
+                        ?>
                     </div>
                 </div>
                 <div class="control-group">
                     <div class="controls">
-                        <input type="button" value="Drop Student" class="btn btn-danger drop_admission">
+                        <input type="button" value="Drop Student" class="btn btn-danger drop_admission" id="drop_button" disabled="disabled">
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="span8">
+    <div class="span4">
+        <div class="widget-box">
+            <div class="widget-title"> <span class="icon"> <i class="icon-align-justify"></i> </span>
+                <h5>Student detail here...</h5>
+            </div>
+            <div class="widget-content" id="student_to_drop_detail">
 
+            </div>
+        </div>
+    </div>
+    <div class="span4">
+        <div class="widget-box">
+            <div class="widget-title"> <span class="icon"> <i class="icon-align-justify"></i> </span>
+                <h5>Address</h5>
+            </div>
+            <div class="widget-content" id="student_to_drop_address">
+
+            </div>
+        </div>
     </div>
     <?php echo form_close();?>
-</div>
-<div class="row-fluid">
-        <?php //$this->load->view('reg_adm/view_student_data'); ?>
 </div>
