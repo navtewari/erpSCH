2ae07d5e877de5fb5cb7355487ae8b0f83a6b187
Dr. Nitin Deepak <nitin.d12@gmail.com>
2018-08-28 15:54:05 +0530

    updates
    updates

--- application/models/My_export_model.php
+++ application/models/My_export_model.php
@@ -13,17 +13,18 @@ class My_export_model extends CI_Model
             $this->load->model('My_error_model', 'error');
         // --------------------
     }  
-	function toCsv($classessid, $cls){
+    function toCsv($classessid, $cls){
         $this->load->dbutil();
         $this->db->where('b.STATUS_', 1);
         $this->db->where('c.CLSSESSID',$classessid);        
         $this->db->order_by('a.FNAME');
-        $this->db->select('a.regid as REG NO, d.CLASSID as Class, UPPER(a.FNAME) as STUDENTNAME, UPPER(a.GENDER) as GENDER, UPPER(a.FATHER) as FATHER, a.F_MOBILE as FATHER_MOBILE, UPPER(a.MOTHER) as MOTHER, a.F_MOBILE as MOTHER_MOBILE, UPPER(a.CATEGORY) as CATEGORY');
+        $this->db->select('a.regid as REG NO, d.CLASSID as Class, UPPER(a.FNAME) as STUDENTNAME, UPPER(a.GENDER) as GENDER, DOB_ as DOB, UPPER(a.FATHER) as FATHER, a.F_MOBILE as FATHER_MOBILE, UPPER(a.MOTHER) as MOTHER, a.F_MOBILE as MOTHER_MOBILE, UPPER(a.CATEGORY) as CATEGORY');
         $this->db->from('master_7_stud_personal a');
         $this->db->join('master_8_stud_academics b', 'a.regid=b.regid');
         $this->db->join('class_3_class_wise_students c', 'a.regid=c.regid');
         $this->db->join('class_2_in_session d', 'c.CLSSESSID=d.CLSSESSID');
         $query = $this->db->get();
+        //echo $this->db->last_query();
         $delimiter = ",";
         $newline = "\r\n";
         $filename = "Class_".$cls."_".$this->session->userdata('school_name').".csv";
