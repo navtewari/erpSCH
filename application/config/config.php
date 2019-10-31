7b4a7cef75b225025c62d3371d8c013e9754af70
Dr. Nitin Deepak <nitin.d12@gmail.com>
2019-10-31 12:34:02 +0530

    updates
    updates

--- application/config/config.php
+++ application/config/config.php
@@ -21,7 +21,7 @@ date_default_timezone_set('Asia/Kolkata');
 */
 $config['base_url'] = 'http://localhost/erpSCH';
 
-define('JS_VERSION_NITIN', 18.8);
+define('JS_VERSION_NITIN', 18.9);
 define('_NITIN_IMG_VERSION_', 1.2);
 
 define('JS_VERSION_NAVEEN', 3.7);
